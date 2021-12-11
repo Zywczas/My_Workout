5d753a2b272e0074827dd44f3c5f0e5b86804274
Piotr Zywczewski <piotr.zywczewski@intive.com>
2021-12-11 09:36:55 +0100

    removed some logs from TimerActivity.kt

--- watch/src/main/java/com/zywczas/myworkout/watch/activities/trainingplan/timer/presentation/TimerActivity.kt
+++ watch/src/main/java/com/zywczas/myworkout/watch/activities/trainingplan/timer/presentation/TimerActivity.kt
@@ -5,11 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
-import android.view.HapticFeedbackConstants
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
-import com.zywczas.common.extetions.logD
 import com.zywczas.common.utils.autoRelease
 import com.zywczas.myworkout.watch.R
 import com.zywczas.myworkout.watch.activities.BaseActivity
@@ -19,16 +16,13 @@ import com.zywczas.myworkout.watch.services.timer.TimerService
 
 class TimerActivity : BaseActivity() {
 
-    init {
-        logD("init aktywnosci") //todo uruchamia sie kilka razy bo serwis tworzy nowe przy kliknieciu w ikonke, do poprawy, moze da sie jakos z back stacku otwierac ta sama
-    }
-
     private var binding: ActivityTimerBinding by autoRelease()
     private val viewModel: TimerViewModel by viewModels { viewModelFactory } //todo chyba do wylotu
     private var timerService: TimerService? = null
     private var isTimerServiceBound = false
     private var isConfigurationChange = false
     private val vibrator by lazy { getSystemService(Vibrator::class.java) }
+    private var isGoingToNextExercise = false
 
     private val timerServiceConnection = object : ServiceConnection { //todo dac to nizej, tam gdzie uzywane i poustawiac wszystkie funkcje
 
@@ -47,7 +41,6 @@ class TimerActivity : BaseActivity() {
 
     private fun setupServiceLiveDataObservers(){
         timerService?.timeLeft?.observe(this){
-            logD("pobiera live date: $it")
             binding.timeLeft.text = it
         }
         timerService?.isAlarmOff?.observe(this) {
@@ -59,9 +52,7 @@ class TimerActivity : BaseActivity() {
     }
 
     private fun turnAlarmOn(){
-        logD("wlaczam wibracje")
         if (vibrator.hasVibrator()) {
-            logD("ma wibrator")
 //        vibrator.vibrate(3000L)//todo deprecated
             vibrator.vibrate(VibrationEffect.createOneShot(3000L, VibrationEffect.DEFAULT_AMPLITUDE)) //todo jest jeszcze sposob by
 //        binding.root.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS) //todo inny sposob na wibracje, i niby nie trzeba permission
@@ -95,6 +86,7 @@ class TimerActivity : BaseActivity() {
             turnAlarmOff()
             unBindAndCloseTimerService()
             goToNextExercise()
+            isGoingToNextExercise = true
             finish()
         }
         binding.goBack.setOnClickListener { onBackPressed() }
@@ -105,7 +97,10 @@ class TimerActivity : BaseActivity() {
     }
 
     private fun turnAlarmOff(){
-        //todo wylaczyc wibracje jesli sa wlaczone
+        //todo wylaczyc wibracje jesli sa wlaczone, sprawdzic czy to ma sens
+        if (vibrator.hasVibrator()) {
+            vibrator.vibrate(VibrationEffect.createOneShot(100L, VibrationEffect.DEFAULT_AMPLITUDE)) //todo sprawdzic czy ma to sens
+        }
     }
 
     private fun unBindAndCloseTimerService(){
@@ -120,6 +115,7 @@ class TimerActivity : BaseActivity() {
     }
 
     private fun goToNextExercise(){
+        //todo zapisanie jakie jest kolejne cwiczenie jesli jeszcze nie zapisalem
         //przejscie do Exercise Activity i wczytanie odpowiedniego cwiczenia i serii todo
     }
 
@@ -127,12 +123,10 @@ class TimerActivity : BaseActivity() {
         val intent = Intent(this, SettingsTimerActivity::class.java)
         startActivity(intent)
     }
-//todo
-    //3. jak serwis skonczy odliczanie to wlaczyc wibracje i obudzic aktywnosc timer, a po wejsciu w aktywnosc wylaczyc serwis
 
     override fun onStop() {
         super.onStop()
-        if (isConfigurationChange.not()){
+        if (isConfigurationChange.not() && isGoingToNextExercise.not()){
             timerService?.goToForegroundService()
         }
     }
