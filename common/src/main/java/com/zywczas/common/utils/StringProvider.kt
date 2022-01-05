877cb5e7252f7b4d8eca4f622c311cfb383c5a9d
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-05 18:34:21 +0100

    fixed showing dates in elements 2

--- common/src/main/java/com/zywczas/common/utils/StringProvider.kt
+++ common/src/main/java/com/zywczas/common/utils/StringProvider.kt
@@ -4,6 +4,8 @@ import androidx.annotation.StringRes
 
 interface StringProvider {
 
-    suspend fun getString(@StringRes resString: Int, vararg args: Any): String
+    suspend fun getString(@StringRes resString: Int): String
+
+    suspend fun getString(@StringRes resString: Int, arg: String): String
 
 }
--- common/src/main/java/com/zywczas/common/utils/StringProviderImpl.kt
+++ common/src/main/java/com/zywczas/common/utils/StringProviderImpl.kt
@@ -1,10 +1,13 @@
 package com.zywczas.common.utils
 
 import android.content.Context
+import androidx.annotation.StringRes
 import javax.inject.Inject
 
 class StringProviderImpl @Inject constructor(private val context: Context) : StringProvider {
 
-    override suspend fun getString(resString: Int, vararg args: Any): String = context.getString(resString, args)
+    override suspend fun getString(resString: Int): String = context.getString(resString)
+
+    override suspend fun getString(@StringRes resString: Int, arg: String): String = context.getString(resString, arg)
 
 }
