8af2477053b58af8ff3bf203fb0fcfed61b0d50d
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-05 17:20:14 +0100

    fixed margin in activity_exercise.xml

--- watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
+++ watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
@@ -13,4 +13,5 @@ todo
 8. crash jak podczas timera wcisnalem guzik back, ale bylem w debuggerze wiec moze to zawadzilo:
 java.lang.RuntimeException: Unable to create service com.zywczas.myworkout.watch.services.timer.TimerService: java.lang.IllegalStateException: Not allowed to start service Intent
 { cmp=com.zywczas.myworkout.watch/.services.timer.TimerService }: app is in background uid UidRecord{17447 u0a65 SVC  idle change:uncached procs:1 seq(0,0,0)}
+9. przy dodawaniu cardio nie oznacza dnia jako rozpoczetego
  */
