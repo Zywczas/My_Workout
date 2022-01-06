523e5e79220703fbc573ec95c26acaba02e8286e
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-06 16:02:29 +0100

    added few comments

--- watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
+++ watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
@@ -4,10 +4,9 @@ package com.zywczas.myworkout.watch.utils
 todo
 0. dac automatyczne przechodzenie z pierwszego ekranu
 2. dac cos ala dialog are you sure przed usuwaniem elementow
-3. Dac ladniejsze ikonki
-4. Progress bar jakos dziwnie wyglada
-10. dac minimalna wysokosc dla kazdego elementu listy, np 60dp - wtedy bedzie mozna usunac te wielkie paddingi
 11. sprawdzic dimensy wszedzie
 12. sprawdzic co to jest to SpeakableTextPresentCheck bo daje ignore we wszystkich layoutow
 13. ustawic detekt tak zeby rowniez przy commitach skanowal kod
+14. w timerze dac odliczanie czasu jako progress bar na zewnatrz kola
+15. dac aktualna godzine w timerze
  */
