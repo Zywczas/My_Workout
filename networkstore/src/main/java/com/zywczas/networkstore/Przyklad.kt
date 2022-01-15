4df23dc0b96edd92874461d09db38657b2771d6d
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-15 17:04:54 +0100

    added Proto dependencies for communication with server

--- networkstore/src/main/java/com/zywczas/networkstore/Przyklad.kt
+++ networkstore/src/main/java/com/zywczas/networkstore/Przyklad.kt
@@ -1,6 +1,22 @@
 package com.zywczas.networkstore
 
+import com.zywczas.networkstore.przykladowe.BookFromProto
+import io.grpc.ManagedChannelBuilder
+
 //todo usunac
+
 class Przyklad {
+
+    private val ksiazka = BookFromProto.Book.newBuilder().apply {
+        isbn = 1L
+        title = "Harry Potter"
+        author = "nie pamietam"
+    }.build()
+
+    private val bookRequest = BookFromProto.GetBookRequest.newBuilder().setIsbn(2L).build()
+
+    private val jakisPort = 123;
+    private val managedChannel = ManagedChannelBuilder.forAddress("adres servera", jakisPort)
+
 
 }
