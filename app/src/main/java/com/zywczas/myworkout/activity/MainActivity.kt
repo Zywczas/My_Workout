e24a6e7ccfab52451688da8489bffe6db7591690
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-16 17:21:02 +0100

    added fragment factory to fragment manager

--- app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
+++ app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.zywczas.myworkout.activity
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
 import com.zywczas.common.di.factories.UniversalFragmentFactory
 import com.zywczas.common.utils.autoRelease
 import com.zywczas.myworkout.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
-//    @Inject
-//    lateinit var fragmentFactory: UniversalFragmentFactory
+    @Inject
+    lateinit var fragmentFactory: UniversalFragmentFactory
+
+    private var binding: ActivityMainBinding by autoRelease()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        AndroidInjection.inject(this)
+        AndroidInjection.inject(this)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         super.onCreate(savedInstanceState)
-        setContent { 
-            Text(text = "Compose dziala!")
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
