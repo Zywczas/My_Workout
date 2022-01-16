6d51a361fb8e15d2edebda34b67bad174be677c9
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-16 18:01:31 +0100

    first layout

--- app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
+++ app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.zywczas.myworkout.activity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.zywczas.common.di.factories.UniversalFragmentFactory
-import com.zywczas.common.utils.autoRelease
-import com.zywczas.myworkout.databinding.ActivityMainBinding
-import dagger.android.AndroidInjection
-import javax.inject.Inject
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.zywczas.myworkout.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var fragmentFactory: UniversalFragmentFactory
-
-    private var binding: ActivityMainBinding by autoRelease()
+//    @Inject
+//    lateinit var fragmentFactory: UniversalFragmentFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
-        supportFragmentManager.fragmentFactory = fragmentFactory
+//        AndroidInjection.inject(this)
+//        supportFragmentManager.fragmentFactory = fragmentFactory
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            MessageCard(Message("Piotr", "ładne body"))
+        }
     }
 
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row() {
+        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "Zdjęcie profilowe")
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(Message("Piotr", "ładne body"))
 }
