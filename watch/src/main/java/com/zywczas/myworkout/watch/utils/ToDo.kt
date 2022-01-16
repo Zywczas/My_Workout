9f20e54f638ff0655d5d3d1e411b4a53495c7543
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-16 16:45:45 +0100

    changed app name in polish

--- watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
+++ watch/src/main/java/com/zywczas/myworkout/watch/utils/ToDo.kt
@@ -8,15 +8,7 @@ todo
 13. ustawic detekt tak zeby rowniez przy commitach skanowal kod
 14. w timerze dac odliczanie czasu jako progress bar na zewnatrz kola
 15. dac aktualna godzine w timerze
-16. zmienic nazwe aplikacji na moj trening
+16. dodac CI
 
-Problem z timerem:
-- jak ekran wygasa to wibracje tez sie zatrzymuja
-- nie tworzy wogole ikonki... servisu... czasami tworzy, czasami nie, mozliwe ze za szybko minimalizuje aktywnosc, zanim zacznie sie serwis i nie odpala funkcji gotoforeground
-- dzisiaj nagle mi sie wibracje wlaczyly jak otworzylem aplikacje i byl widok cwiczenia
-- ram mialem blad ze nie mozna aktywowac serwisu z backgroundu
-2 glowne problemy:
-- jak juz raz stworzy ikonke ongoing activity to drugi raz jej nie uruchamia po ponownym przejsciu w background
-- nie przywoluje aktywnosci po zakonczeniu czasu - bring aktivity to fron cos nie dziala
 
  */
