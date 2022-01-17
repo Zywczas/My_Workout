67bcf049e005240f02185998b064756efe99410b
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-17 19:49:38 +0100

    modifiers

--- app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
+++ app/src/main/java/com/zywczas/myworkout/activity/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zywczas.myworkout.R
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +35,17 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row() {
-        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "Zdjęcie profilowe")
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Zdjęcie profilowe",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Column() {
             Text(text = msg.author)
+            Spacer(modifier = Modifier.width(10.dp))
             Text(text = msg.body)
 
         }
