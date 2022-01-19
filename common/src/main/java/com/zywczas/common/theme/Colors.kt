0c694de47ae4621ec049c2e849517fdf1230c66e
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-01-19 20:16:13 +0100

    moved colors to compose

---
+++ common/src/main/java/com/zywczas/common/theme/Colors.kt
@@ -0,0 +1,17 @@
+package com.zywczas.common.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorLightGrayDarkMode = Color(0xFF9A9797)
+val ColorDeepPurple900 = Color(0xFF311b92)
+
+//todo pomyslec czy dac tu przyrostki DarkMode...
+val ColorPrimary = Color(0xFF0072F8)
+val ColorLogoFirst = Color(0xFF0E7DD4)
+val ColorLogoSecond = Color(0xFF00FF8B)
+val ColorExerciseDoneTextDarkMode = ColorLightGrayDarkMode
+val ColorExerciseDoneBackgroundDarkMode = Color(0xFF33374E)
+val ColorSettingsItemBackground = Color(0xFF28950C)
+val ColorDeleteItemBackground = ColorDeepPurple900
+
+
