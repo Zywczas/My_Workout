5c78a44f64d03c60001e1cb286f7f4b3b50ccc6c
PiotrZywczewski <piotr.zywczewski@gmail.com>
2023-04-10 11:08:05 +0200

    Deleted unsused libs dependencies

--- common/src/main/java/com/zywczas/common/extetions/Activity.kt
+++ common/src/main/java/com/zywczas/common/extetions/Activity.kt
@@ -5,4 +5,3 @@ import android.widget.Toast
 import androidx.annotation.StringRes
 
 fun Activity.showToast(@StringRes text: Int) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-fun Activity.showToast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
